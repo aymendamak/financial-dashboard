8df00a4f79a24788f90b71f7671d115ca828bc73
aymendamak <aymen.damak@yahoo.fr>
2025-02-01 10:19:27 +0100

    Mise à jour du composant TopBar pour personnaliser le message de bienvenue et afficher la date actuelle dans un format lisible.

--- src/components/TopBar/top-bar.tsx
+++ src/components/TopBar/top-bar.tsx
@@ -5,9 +5,17 @@ const TopBar = () => {
     <div className="border-b mb-4 px-4 mt-2 pb-2 border-stone-200">
       <div className="flex items-center justify-between p-0.5">
         <div>
-          <span className="text-sm font-bold block">Good morning, Tom!</span>
-          <span className="text-sx block text-stone-500">
-            Tuesday, Aug 8th 2024
+          <span className="text-md font-bold block">
+            Good morning,{" "}
+            <span className="font-bold text-cyan-500 ">Aymen</span> !
+          </span>
+          <span className="text-sm block text-stone-500">
+            {new Date().toLocaleDateString(undefined, {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
           </span>
         </div>
 
